4f56b6355dfd15cc0e55df77b4f5adc08cd7c793
nikilok <nikilok@gmail.com>
2025-06-05 14:16:49 +0100

    basic markup

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,3 +1,26 @@
 export default function Chat() {
-	return <main />;
+	return (
+		<main className="flex flex-col h-screen">
+			{/* Chat Container */}
+			<div className="flex flex-col justify-end flex-grow overflow-y-auto">
+				<div className="">Today, 1:06PM</div>
+				<div className="">Hey did you go someplace today ?</div>
+				<div className="">Yes I went to Oxford, how about you ?</div>
+				<div className="">Was at home mostly</div>
+			</div>
+			{/* Chat toolbar */}
+			<div className="bg-gray-800 h-15 flex items-center justify-center">
+				<div className="bg-white border border-gray-300 rounded-full h-10 w-[90%] p-[10px]">
+					<form className="flex items-center h-full">
+						<input
+							// biome-ignore lint/a11y/noAutofocus: <explanation>
+							autoFocus
+							className="w-full h-full border-none outline-none ring-0 shadow-none text-gray-900 placeholder-gray-500 px-3"
+							placeholder="Type a message..."
+						/>
+					</form>
+				</div>
+			</div>
+		</main>
+	);
 }
