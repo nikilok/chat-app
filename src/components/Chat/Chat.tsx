2774f29029bce67096daad471a1857c3a5b4c83e
nikilok <nikilok@gmail.com>
2025-06-10 14:13:41 +0100

    get rid of tailwind and go back to css modules

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,21 +1,29 @@
+import styles from "./Chat.module.css";
+
 export default function Chat() {
 	return (
-		<main className="flex flex-col h-screen bg-gray-500">
+		<main style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
 			{/* Chat Container */}
-			<div className="flex flex-col justify-end flex-grow overflow-y-auto">
-				<div className="">Today, 1:06PM</div>
-				<div className="bg-red-200">Hey did you go someplace today ?</div>
-				<div className="">Yes I went to Oxford, how about you ?</div>
-				<div className="">Was at home mostly</div>
+			<div className={styles.chatContainer}>
+				<div>Today, 1:06PM</div>
+				<div className={styles.chatBubble}>
+					Hey did you go someplace today Hey did you go someplace today ?
+				</div>
+				<div className={styles.chatBubbleOther}>
+					Yes I went to Oxford, how about you ?
+				</div>
+				<div className={styles.chatBubble}>Was at home mostly</div>
 			</div>
 			{/* Chat toolbar */}
-			<div className="bg-gray-800 h-15 flex items-center justify-center">
-				<div className="bg-white border border-gray-300 rounded-full h-10 w-[90%] p-[10px]">
-					<form className="flex items-center h-full">
+			<div className={styles.chatToolbar}>
+				<div className={styles.chatInputContainer}>
+					<form
+						style={{ display: "flex", alignItems: "center", height: "100%" }}
+					>
 						<input
 							// biome-ignore lint/a11y/noAutofocus: we like to autofocus as this is a chat app
 							autoFocus
-							className="w-full h-full border-none outline-none ring-0 shadow-none text-gray-900 placeholder-gray-500 px-3"
+							className={styles.chatInput}
 							placeholder="Type a message..."
 						/>
 					</form>
